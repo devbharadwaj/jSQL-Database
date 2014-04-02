6bbde821626392e978a1a4d6bff35f5da1386df0
cse562 <devbharadwaj87@gmail.com>
2014-04-02 03:47:52 -0400

    Main class

--- src/edu/buffalo/cse562/Main.java
+++ src/edu/buffalo/cse562/Main.java
@@ -1,13 +1,25 @@
 package edu.buffalo.cse562;
 
+import java.util.ArrayList;
+
 public class Main {
 
 	public static void main(String[] args) {
+		String dataDir;
+		ArrayList<String> sqlFiles = new ArrayList<>();
+		boolean getSqlFiles = false;
+		
 		for (int i = 0; i < args.length; i++) {
-			if (args[i] == "--data") {
-				  
+			if (getSqlFiles) {
+				sqlFiles.add(args[i]);
+			}
+			else if (args[i] == "--data") {
+				dataDir = args[i+1];
+				getSqlFiles = true;
 			}
 		}
+		
+		
 
 	}
 
