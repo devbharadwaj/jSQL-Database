4b3c211452222d335f6a8071220f12931c9cf185
cse562 <dbharadw@buffalo.edu>
2014-04-02 03:30:07 -0400

    first commit

---
+++ src/edu/buffalo/cse562/Main.java
@@ -0,0 +1,14 @@
+package edu.buffalo.cse562;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < args.length; i++) {
+			if (args[i] == "--data") {
+				
+			}
+		}
+
+	}
+
+}
